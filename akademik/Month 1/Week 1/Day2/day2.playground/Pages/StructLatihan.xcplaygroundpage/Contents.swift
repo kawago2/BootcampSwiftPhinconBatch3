4214b338e61dd87bde7ffed28ba8daa10bd35bbc
Kaleb Edward <itkawago@gmail.com>
2023-10-24 13:30:36 +0700

    day 2 at 13.30

--- akademik/Month 1/Week 1/Day2/day2.playground/Pages/StructLatihan.xcplaygroundpage/Contents.swift
+++ akademik/Month 1/Week 1/Day2/day2.playground/Pages/StructLatihan.xcplaygroundpage/Contents.swift
@@ -33,7 +33,6 @@ SSN: \(ssn)
     }
 }
 
-
 let personInfo = Person1(name: "Kaleb Gomgom", age: 22, address: "JL Koja", identity: Identity(idNumber: "12345", passportNumber: "ABC123", ssn: "123-45-6789"))
 
 personInfo.details()
@@ -52,19 +51,38 @@ struct Person: Equatable {
     var name: String
     var age: Int
     var role: Identity2
+    var income: Int
+    var residu: Int
+    var history: [Int] = [Int]()
     
-
+    init(name: String, age: Int, role:Identity2, income:Int, residu: Int = 0,history: [Int] = []) {
+        self.name = name
+        self.age = age
+        self.role = role
+        self.income = income
+        self.residu = residu
+        self.history = history
+    }
+     
     static func == (lhs: Person, rhs: Person) -> Bool {
         return lhs.name == rhs.name &&
         lhs.age == rhs.age &&
         lhs.role == rhs.role
     }
+    
+    static func description() {
+        print("desc")
+    }
+    
     func details(){
         print("""
-Name: \(name)
-Age: \(age)
-Role: \(role)
-"""
+            Name: \(name)
+            Age: \(age)
+            Role: \(role)
+            Income: \(income)
+            Residu: \(residu)
+            History: \(history)
+            """
         )
     }
     
@@ -80,17 +98,34 @@ Role: \(role)
         self.role = .teacher
     }
     
+    mutating func Paying(amount: Int) {
+        if self.income >= amount {
+            history.append(amount)
+            var result = 0
+            for i in history {
+                result += i
+            }
+            self.residu = self.income - result
+        } else {
+            fatalError("Pay to high")
+        }
+       
+    }
 }
 
 
+Person.description()
+
 // Contoh penggunaan struct
-var person1 = Person(name: "Kaleb", age: 30, role: .employee)
-let person2 = Person(name: "Kaleb", age: 30, role: .employee)
-let person3 = Person(name: "Kaleb", age: 30, role: .teacher)
+var person1 = Person(name: "Kaleb", age: 30, role: .employee, income: 10000)
+let person2 = Person(name: "Kaleb", age: 30, role: .employee, income: 20000)
+let person3 = Person(name: "Kaleb", age: 30, role: .teacher, income: 30000)
 
 person1.changeName(name: "Edward")
 person1.changeAge(age: 27)
 person1.isTecher()
+person1.Paying(amount: 5000)
+person1.Paying(amount: 200)
 
 person1.details()
 person2.details()
