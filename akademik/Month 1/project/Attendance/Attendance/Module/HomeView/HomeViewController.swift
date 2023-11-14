e217ea4119d1c8bec4add9a63f563d29f7d37fd8
Kaleb Edward <itkawago@gmail.com>
2023-11-14 13:21:54 +0700

    [FEAT] home screen fix up

--- akademik/Month 1/project/Attendance/Attendance/Module/HomeView/HomeViewController.swift
+++ akademik/Month 1/project/Attendance/Attendance/Module/HomeView/HomeViewController.swift
@@ -8,22 +8,78 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    @IBOutlet weak var circleView: UIImageView!
+    @IBOutlet weak var bellView: UIImageView!
+    @IBOutlet weak var clockLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var isCheckInLabel: UILabel!
+    @IBOutlet weak var isCheckInCircle: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var checkView: UIView!
+    
+    let dataArray = [
+        ["title": "PT. Phincon", "desc": "Office. 88 @Kasablanka Office Tower 18th Floor"],
+        ["title": "Telkomsel Smart Office", "desc": "Jl. Jend. Gatot Subroto Kav. 52. Jakarta Selatan"],
+        ["title": "Rumah", "desc": "Jakarta"]
+    ]
 
+    
+    var isCheckIn = false
+    var timer: Timer?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
+        buttonEvent()
+        setupRealTimeClock()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func buttonEvent() {
+        isCheckInCircle.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(checkToggle))
+        isCheckInCircle.addGestureRecognizer(tapGesture)
     }
-    */
+    
+    @objc func checkToggle() {
+        self.isCheckIn = !isCheckIn
+        updateCheck()
+    }
+    
+    func setupUI() {
+        circleView.tintColor = .white.withAlphaComponent(0.05)
+        checkView.makeCornerRadius(20)
+        updateCheck()
+    }
+    
+    func updateCheck() {
+        isCheckInLabel.text = isCheckIn ? "CHECK OUT" : "CHECK IN"
+        isCheckInCircle.tintColor = isCheckIn ? UIColor.systemOrange : UIColor.systemGreen
+    }
+    
+    func setupRealTimeClock() {
+        updateTimeLabels()
 
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimeLabels), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimeLabels() {
+        let currentDate = Date()
+
+        // Format date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d MMMM yyyy"
+        let formattedDate = dateFormatter.string(from: currentDate)
+
+        // Format time
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateFormat = "HH:mm"
+        let formattedTime = timeFormatter.string(from: currentDate)
+
+        // Update labels
+        dateLabel.text = "\(formattedDate)"
+        clockLabel.text = "Hour: \(formattedTime)\n"
+    }
+    
+    
 }
---
+++ akademik/Month 1/project/Attendance/Attendance/Module/HomeView/TableCell/LocationCell.swift
@@ -0,0 +1,23 @@
+//
+//  LocationCell.swift
+//  Attendance
+//
+//  Created by Phincon on 14/11/23.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
