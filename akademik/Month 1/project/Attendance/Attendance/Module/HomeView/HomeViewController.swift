f24bb840cb300fb951e1f14283633e9927be5279
Kaleb Edward <itkawago@gmail.com>
2023-11-14 16:00:33 +0700

    [FEAT] logic check in n out

--- akademik/Month 1/project/Attendance/Attendance/Module/HomeView/HomeViewController.swift
+++ akademik/Month 1/project/Attendance/Attendance/Module/HomeView/HomeViewController.swift
@@ -13,50 +13,61 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var clockLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var isCheckInLabel: UILabel!
-    @IBOutlet weak var isCheckInCircle: UIImageView!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var checkView: UIView!
+    @IBOutlet weak var circleButton: UIView!
     
-    let dataArray = [
-        ["title": "PT. Phincon", "desc": "Office. 88 @Kasablanka Office Tower 18th Floor"],
-        ["title": "Telkomsel Smart Office", "desc": "Jl. Jend. Gatot Subroto Kav. 52. Jakarta Selatan"],
-        ["title": "Rumah", "desc": "Jakarta"]
+    let locationArray = [
+        ["title": "PT. Phincon", "desc": "Office. 88 @Kasablanka Office Tower 18th Floor", "img": "id_1"],
+        ["title": "Telkomsel Smart Office", "desc": "Jl. Jend. Gatot Subroto Kav. 52. Jakarta Selatan", "img": "id_2"],
+        ["title": "Rumah", "desc": "Jakarta", "img": "id_3"]
     ]
-
     
+    var locationSelected: [String: String] = [:]
     var isCheckIn = false
     var timer: Timer?
-    
+    var selectedCell = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         buttonEvent()
         setupRealTimeClock()
+        setDefaultSelected()
     }
     
     func buttonEvent() {
-        isCheckInCircle.isUserInteractionEnabled = true
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(checkToggle))
-        isCheckInCircle.addGestureRecognizer(tapGesture)
+        isCheckInLabel.addGestureRecognizer(tapGesture)
     }
-    
+
     @objc func checkToggle() {
         self.isCheckIn = !isCheckIn
         updateCheck()
+        tableView.reloadData()
+        if isCheckIn == false {
+            print(self.selectedCell)
+            setDefaultSelected()
+        }
     }
     
     func setupUI() {
+        circleButton.layer.cornerRadius = circleButton.bounds.width / 2.0
+        circleButton.clipsToBounds = true
         circleView.tintColor = .white.withAlphaComponent(0.05)
         checkView.makeCornerRadius(20)
         updateCheck()
+        tableView.registerCellWithNib(LocationCell.self)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     func updateCheck() {
         isCheckInLabel.text = isCheckIn ? "CHECK OUT" : "CHECK IN"
-        isCheckInCircle.tintColor = isCheckIn ? UIColor.systemOrange : UIColor.systemGreen
+        circleButton.backgroundColor = isCheckIn ? UIColor.systemOrange : UIColor.systemGreen
     }
-    
+
+
     func setupRealTimeClock() {
         updateTimeLabels()
 
@@ -81,5 +92,51 @@ class HomeViewController: UIViewController {
         clockLabel.text = "Hour: \(formattedTime)\n"
     }
     
+    func setDefaultSelected() {
+        let defaultIndexPath = IndexPath(row: selectedCell, section: 0)
+        tableView.selectRow(at: defaultIndexPath, animated: false, scrollPosition: .none)
+        tableView.delegate?.tableView?(tableView, didSelectRowAt: defaultIndexPath)
+    }
     
+}
+
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return isCheckIn ? 1 : locationArray.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let index = indexPath.row
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath) as! LocationCell
+        let location = isCheckIn ? locationSelected : locationArray[index]
+        if isCheckIn {
+            let defaultIndexPath = IndexPath(row: 0, section: 0)
+            tableView.selectRow(at: defaultIndexPath, animated: false, scrollPosition: .none)
+            tableView.delegate?.tableView?(tableView, didSelectRowAt: defaultIndexPath)
+        }
+        cell.context = isCheckIn ? true : false
+        cell.initData(title: location["title"] ?? "", desc: location["desc"] ?? "", img: location["img"] ?? "image_not_available")
+        
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if isCheckIn == false {
+            let index = indexPath.row
+            self.selectedCell = index
+            let locationSelected = locationArray[index]
+            self.locationSelected = locationSelected
+        }
+
+    }
 }
--- akademik/Month 1/project/Attendance/Attendance/Module/HomeView/TableCell/LocationCell.swift
+++ akademik/Month 1/project/Attendance/Attendance/Module/HomeView/TableCell/LocationCell.swift
@@ -1,23 +1,44 @@
-//
-//  LocationCell.swift
-//  Attendance
-//
-//  Created by Phincon on 14/11/23.
-//
-
 import UIKit
 
 class LocationCell: UITableViewCell {
-
+    @IBOutlet weak var squareView: UIView!
+    @IBOutlet weak var imageeView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    
+    var context = false
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setupUI()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
+        if context {
+            squareView.backgroundColor = selected ? UIColor.systemOrange : UIColor.white
+        } else {
+            squareView.backgroundColor = selected ? UIColor(named: "MainColor") : UIColor.white
+            titleLabel.textColor = selected ? UIColor.white : UIColor(named: "PrimaryTextColor")
+            descLabel.textColor = selected ? UIColor.white : UIColor.black
+        }
+       
     }
+    
+    
+    func setupUI() {
+        squareView.makeCornerRadius(10)
+        selectionStyle = .none
+    }
+    
+    func initData(title: String, desc: String, img: String) {
+        titleLabel.text = title
+        descLabel.text = desc
+        imageeView.image = UIImage(named: img)
+    }
+    
+    
     
 }
