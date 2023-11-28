8b9e3336c67386f6f6dedf1051ff19251cd3f920
Kaleb Edward <itkawago@gmail.com>
2023-11-28 14:04:48 +0700

    [NEW] New project for tes

---
+++ akademik/Month 1/project/Tst/Tst/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tst
+//
+//  Created by Phincon on 28/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
