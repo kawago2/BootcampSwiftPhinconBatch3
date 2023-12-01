06f08918a247067f56f364cdfa255fcf7e8f5556
Kaleb Edward <itkawago@gmail.com>
2023-12-01 16:20:05 +0700

    [FEAT] inprogress autoscroll

--- akademik/Month 1/project/Fintech/Fintech/Module/OnboardingView/OnboardingViewController.swift
+++ akademik/Month 1/project/Fintech/Fintech/Module/OnboardingView/OnboardingViewController.swift
@@ -12,13 +12,23 @@ class OnboardingViewController: UIViewController {
     @IBOutlet weak var skipButton: UIButton!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var nextButton: UIButton!
     
     var onboardingList: [Onboarding] = []
+    var numberOfPages: Int  {
+        return self.onboardingList.count
+    }
+    var timer: Timer?
+    var currentPages = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadData()
         setupUI()
+        startAutoplay()
     }
 
     
@@ -26,6 +36,8 @@ class OnboardingViewController: UIViewController {
         cardView.roundCorners(corners: [.allCorners], cornerRadius: 30)
         skipButton.backgroundColor = .white.withAlphaComponent(0.10)
         skipButton.roundCorners(corners: [.allCorners], cornerRadius: 30)
+        descriptionLabel.textColor = UIColor(hex: "13095E")?.withAlphaComponent(0.80)
+        titleLabel.textColor = UIColor(hex: "13095E")
         
         collectionView.registerCellWithNib(OnboardingCell.self)
         collectionView.delegate = self
@@ -38,6 +50,32 @@ class OnboardingViewController: UIViewController {
             Onboarding(image: "onboard-2", title: "Gain total control of your money", description: "Track your transactions easily, with categories and financial reports."),
             Onboarding(image: "onboard-3", title: "Plan ahead and manage your money better", description: "Set up your budget for each category so you're in control. Track categories where you spend the most money.")
         ]
+    }
+    
+    private func startAutoplay() {
+        timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(autoplay), userInfo: nil, repeats: true)
+    }
+    
+    @objc func autoplay() {
+       currentPages = (currentPages + 1) % numberOfPages
+       pageControl.currentPage = currentPages
+       let newOffset = CGPoint(x: collectionView.frame.width * CGFloat(currentPages), y: collectionView.contentOffset.y)
+       collectionView.setContentOffset(newOffset, animated: true)
+       collectionView.layoutIfNeeded()
+       updateUIForCurrentPage()
+   }
+    
+    private func pageControlClicked() {
+       let currentPage = pageControl.currentPage
+       let indexPath = IndexPath(item: currentPage, section: 0)
+       collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+       updateUIForCurrentPage()
+   }
+    
+    private func updateUIForCurrentPage() {
+        guard currentPages < onboardingList.count else { return }
+        titleLabel.text = onboardingList[currentPages].title ?? ""
+        descriptionLabel.text = onboardingList[currentPages].description ?? ""
     }
 }
 
@@ -53,6 +91,13 @@ extension OnboardingViewController: UICollectionViewDataSource, UICollectionView
         cell.configureImage(image: onboarding.image ?? "image_not_available")
         
         return cell
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageWidth = scrollView.frame.width
+        let currentPage = Int(scrollView.contentOffset.x / pageWidth)
+        pageControl.currentPage = currentPage
+        updateUIForCurrentPage()
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
