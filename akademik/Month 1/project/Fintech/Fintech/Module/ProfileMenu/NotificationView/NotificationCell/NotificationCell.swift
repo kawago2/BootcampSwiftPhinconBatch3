607bdd1fb4fa2524d372cb62d2a4f8c49284f000
Kaleb Edward <itkawago@gmail.com>
2023-12-08 13:50:48 +0700

    [NEW] setup ui notification view

---
+++ akademik/Month 1/project/Fintech/Fintech/Module/ProfileMenu/NotificationView/NotificationCell/NotificationCell.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationCell.swift
+//  Fintech
+//
+//  Created by Phincon on 08/12/23.
+//
+
+import UIKit
+
+class NotificationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ akademik/Month 1/project/Fintech/Fintech/Module/ProfileMenu/NotificationView/NotificationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationViewController.swift
+//  Fintech
+//
+//  Created by Phincon on 08/12/23.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        
+    }
+}
