65ee5c0fff2a195b073d99523dedc1fb5fddadf8
Kaleb Edward <itkawago@gmail.com>
2023-12-11 14:29:35 +0700

    [UI] Logic different color on one label

--- akademik/Month 1/project/Fintech/Fintech/Module/ChartMenu/InsightView/InsightViewController.swift
+++ akademik/Month 1/project/Fintech/Fintech/Module/ChartMenu/InsightView/InsightViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 class InsightViewController: BaseViewController {
 
     @IBOutlet weak var navigationBar: NavigationBar!
-    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var topView: UIView!
     @IBOutlet weak var balanceLabel: UILabel!
     @IBOutlet weak var differentLabel: UILabel!
+    @IBOutlet weak var threadLabel: UILabel!
+    @IBOutlet weak var cardView: UIView!
     
+    private let vsLastWeekString = " vs last week"
+    private let percentageValue = 4.3
     let viewModel = InsightViewModel()
     
     override func viewDidLoad() {
@@ -22,8 +26,25 @@ class InsightViewController: BaseViewController {
     }
     
     private func setupUI() {
-        cardView.roundCorners(corners: [.allCorners], cornerRadius: 30)
+        topView.roundCorners(corners: [.allCorners], cornerRadius: 30)
+        cardView.roundCorners(corners: [.topLeft, .topRight], cornerRadius: 20)
         navigationBar.centerLabel.textColor = .white
         navigationBar.titleNavigationBar = viewModel.titleNavigationBar
+        threadLabel.textColor = .white.withAlphaComponent(0.70)
+        
+        setupLabel()
     }
+    
+    private func setupLabel() {
+        let percentageString = String(format: "+%.1f%%", percentageValue)
+        let percentageAttributedString = viewModel.makeAttributedString(with: percentageString, color: .green)
+        let vsLastWeekAttributedString = viewModel.makeAttributedString(with: vsLastWeekString, color: .white)
+
+        let combinedAttributedString = NSMutableAttributedString()
+        combinedAttributedString.append(percentageAttributedString)
+        combinedAttributedString.append(vsLastWeekAttributedString)
+
+        differentLabel.attributedText = combinedAttributedString
+    }
+
 }
--- akademik/Month 1/project/Fintech/Fintech/Module/ChartMenu/InsightView/InsightViewModel.swift
+++ akademik/Month 1/project/Fintech/Fintech/Module/ChartMenu/InsightView/InsightViewModel.swift
@@ -1,9 +1,17 @@
 import UIKit
 
+enum AttributeKey {
+    static let foregroundColor = NSAttributedString.Key.foregroundColor
+}
 
 class InsightViewModel {
     
     let titleNavigationBar = "Insights"
     
-    
+    func makeAttributedString(with text: String, color: UIColor) -> NSAttributedString {
+        let attributes: [NSAttributedString.Key: Any] = [
+            AttributeKey.foregroundColor: color,
+        ]
+        return NSAttributedString(string: text, attributes: attributes)
+    }
 }
