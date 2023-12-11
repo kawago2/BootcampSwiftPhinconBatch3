ad42dbcc55aeb93dd55319b4cafecdfdf478d4f2
Kaleb Edward <itkawago@gmail.com>
2023-12-11 09:22:47 +0700

    [NEW] Setup Insight view

---
+++ akademik/Month 1/project/Fintech/Fintech/Module/ChartMenu/InsightView/InsightViewController.swift
@@ -0,0 +1,20 @@
+//
+//  InsightViewController.swift
+//  Fintech
+//
+//  Created by Phincon on 11/12/23.
+//
+
+import UIKit
+
+class InsightViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        
+    }
+}
