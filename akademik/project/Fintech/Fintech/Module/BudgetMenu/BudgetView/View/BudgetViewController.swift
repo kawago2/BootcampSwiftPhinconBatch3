a04acadd019f46b55d964d788f7e9d591509155b
kawago2 <itkawago@gmail.com>
2023-12-14 16:15:27 +0700

    [NEW] Setup Budget Menu

---
+++ akademik/project/Fintech/Fintech/Module/BudgetMenu/BudgetView/View/BudgetViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BudgetViewController.swift
+//  Fintech
+//
+//  Created by Phincon on 14/12/23.
+//
+
+import UIKit
+
+class BudgetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
