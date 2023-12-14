f008d8e2a37381cf1998fcd85a566c6fc47ea576
kawago2 <itkawago@gmail.com>
2023-12-14 16:27:42 +0700

    [IMP] Setup pop up if first time

--- akademik/project/Fintech/Fintech/Module/BudgetMenu/BudgetView/View/BudgetViewController.swift
+++ akademik/project/Fintech/Fintech/Module/BudgetMenu/BudgetView/View/BudgetViewController.swift
@@ -1,10 +1,3 @@
-//
-//  BudgetViewController.swift
-//  Fintech
-//
-//  Created by Phincon on 14/12/23.
-//
-
 import UIKit
 
 class BudgetViewController: UIViewController {
@@ -12,4 +5,34 @@ class BudgetViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        showPopUp(titleButton: "Create a New Budget", image: CustomImage.imageBudgetPop, getDefaultManager: UserDefaultsManager.shared.getFirstBudget(), setDefaultManager: {
+            UserDefaultsManager.shared.setFirstBudget(false)
+        })
+    }
 }
+
+// MARK: - Setup PopUp
+extension BudgetViewController: CustomPopUpViewDelegate {
+    func showPopUp(titleButton: String, image: String, getDefaultManager: Bool, setDefaultManager: @escaping () -> Void) {
+        guard !getDefaultManager else {
+            return
+        }
+
+        let vc = CustomPopUpViewController()
+        vc.configurePopUp(item: CustomPopUp(image: image, titleButton: titleButton))
+        vc.delegate = self
+        vc.modalTransitionStyle = .crossDissolve
+        vc.modalPresentationStyle = .overFullScreen
+        present(vc, animated: true) {
+            setDefaultManager()
+        }
+    }
+
+    func buttonLogic() {
+        
+    }
+}
+
