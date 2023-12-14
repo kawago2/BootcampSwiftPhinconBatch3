9a40382f59c7a69cd9ef58d32c8f9fd1317bf7d2
kawago2 <90557836+kawago2@users.noreply.github.com>
2023-12-14 15:30:48 +0700

    Delete akademik/Month 1/project/Fintech/Pods directory

--- akademik/Month 1/project/Fintech/Pods/Target Support Files/Pods-Fintech-FintechUITests/Pods-Fintech-FintechUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Fintech_FintechUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Fintech_FintechUITests
-@end
--- akademik/Month 1/project/Fintech/Pods/Target Support Files/Pods-FintechTests/Pods-FintechTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FintechTests : NSObject
-@end
-@implementation PodsDummy_Pods_FintechTests
-@end
