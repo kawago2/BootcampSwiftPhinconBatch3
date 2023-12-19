c60e75135efa062a0d6071c912f3199621f3398f
kawago2 <itkawago@gmail.com>
2023-12-19 12:57:58 +0700

    [ETC] Remove main xib

--- akademik/project/Attendance/Attendance/App/AppDelegate.swift
+++ akademik/project/Attendance/Attendance/App/AppDelegate.swift
@@ -11,7 +11,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
+        _ = FirebaseManager.shared
 #if DEBUG
         NFX.sharedInstance().start()
 #endif
