397e45814828ec8edfe872f215dcb73a86454501
kawago2 <itkawago@gmail.com>
2023-12-19 13:57:38 +0700

    [IMP] fix tabbarview

--- akademik/project/profileUI/profileUI/Module/MainTabBar/TabBarViewController.swift
+++ akademik/project/profileUI/profileUI/Module/MainTabBar/TabBarViewController.swift
@@ -2,56 +2,42 @@ import UIKit
 
 class TabBarViewController: UITabBarController {
     
-    let homeViewController = HomeViewController()
-    let historyViewController = HistoryViewController()
-    let timesheetViewController = TimesheetViewController()
+    let dashboardViewController = DashboardViewController()
     let profileViewController = ProfileViewController()
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUITabBarItems()
         configureTab()
-        configureAppearance()
         setFirstFocus(index: 0)
     }
     
     func configureUITabBarItems() {
-        let homeTabBarItem = UITabBarItem(title: "Home", image: Icons.home, tag: 0)
-        let historyTabBarItem = UITabBarItem(title: "History", image: Icons.history, tag: 1)
-        let timesheetTabBarItem = UITabBarItem(title: "Timesheet", image: Icons.timesheet, tag: 2)
-        let profileTabBarItem = UITabBarItem(title: "Profile", image: Icons.profile, tag: 3)
+        // Create separate UITabBarItem objects for each view controller
+        let dashboardTabBarItem = UITabBarItem(title: "Dashboard", image: IconSystem.dashboard, tag: 0)
+        let profileTabBarItem = UITabBarItem(title: "Profile", image: IconSystem.profile, tag: 0)
         
-        homeViewController.tabBarItem = homeTabBarItem
-        historyViewController.tabBarItem = historyTabBarItem
-        timesheetViewController.tabBarItem = timesheetTabBarItem
+        dashboardViewController.tabBarItem = dashboardTabBarItem
         profileViewController.tabBarItem = profileTabBarItem
         
-        viewControllers = [homeViewController, historyViewController, timesheetViewController, profileViewController]
-    }
-    
-    func configureAppearance() {
-        let normalTextAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(named: "unselectColor") ?? UIColor.black]
-        let selectedTextAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(named: "MainColor") ?? UIColor.black]
+        viewControllers = [ dashboardViewController, profileViewController]
         
-        UITabBarItem.appearance().setTitleTextAttributes(normalTextAttributes, for: .normal)
-        UITabBarItem.appearance().setTitleTextAttributes(selectedTextAttributes, for: .selected)
+        // Customize appearance for tab bar items
+        UITabBarItem.appearance().setTitleTextAttributes([.foregroundColor: UIColor.black], for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes([.foregroundColor: UIColor(named: "MainColor") ?? UIColor.black], for: .selected)
+        UITabBar.appearance().tintColor = UIColor(named: "MainColor") ?? UIColor.black
+        UITabBar.appearance().barTintColor = UIColor(named: "ThirdColor") ?? UIColor.white
         
-        let iconColor = UIColor(named: "MainColor") ?? UIColor.black
-        UITabBar.appearance().tintColor = iconColor
-        
-        let bgColor = UIColor.white
-        UITabBar.appearance().barTintColor = bgColor
-        
+        // Hide the separator and make the background clear
         UITabBar.appearance().backgroundImage = UIImage()
         UITabBar.appearance().shadowImage = UIImage()
+        
     }
     
     func configureTab() {
-        setViewControllers(viewControllers, animated: true)
+        setViewControllers([dashboardViewController, profileViewController], animated: true)
     }
-    
-    func setFirstFocus(index: Int) {
+    func setFirstFocus(index: Int){
         selectedIndex = index
     }
 }
