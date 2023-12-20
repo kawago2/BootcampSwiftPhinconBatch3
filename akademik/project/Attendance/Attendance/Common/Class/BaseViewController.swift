e77205a893e023b767004d099e2bb83efed2993f
kawago2 <itkawago@gmail.com>
2023-12-20 14:44:24 +0700

    [NEW] create baseview

---
+++ akademik/project/Attendance/Attendance/Common/Class/BaseViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BaseViewController.swift
+//  Attendance
+//
+//  Created by Phincon on 20/12/23.
+//
+
+import UIKit
+import RxSwift
+
+class BaseViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    public let disposeBag = DisposeBag()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
