e077fd493495570f95c6fb651ece3f9c3b59030b
kawago2 <itkawago@gmail.com>
2023-12-20 14:37:00 +0700

    [ETC] remove unused package

--- akademik/project/Attendance/Attendance/App/AppDelegate.swift
+++ akademik/project/Attendance/Attendance/App/AppDelegate.swift
@@ -1,9 +1,6 @@
 import UIKit
 import FirebaseCore
 import Reachability
-#if DEBUG
-import netfox
-#endif
 
 
 @main
@@ -12,9 +9,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         _ = FirebaseManager.shared
-#if DEBUG
-        NFX.sharedInstance().start()
-#endif
         return true
     }
 
